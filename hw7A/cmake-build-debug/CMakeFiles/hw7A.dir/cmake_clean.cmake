ea12f274d34063499ad782d98f05dd8a79823ccf
Weibin Ma <43023438+BruceMT@users.noreply.github.com>
2020-07-18 16:51:44 -0400

    Add files via upload
    hw7  A i

---
+++ hw7A/cmake-build-debug/CMakeFiles/hw7A.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw7A.dir/main.cpp.obj"
+  "hw7A.pdb"
+  "hw7A.exe"
+  "hw7A.exe.manifest"
+  "libhw7A.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw7A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
