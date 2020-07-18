9e092dbfe69a1f92afc2c93a3aac0f32136b27ed
Weibin Ma <43023438+BruceMT@users.noreply.github.com>
2020-07-18 17:02:43 -0400

    Add files via upload

---
+++ c++HW9/cmake-build-debug/CMakeFiles/c__HW9.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c__HW9.dir/main.cpp.obj"
+  "c__HW9.pdb"
+  "c__HW9.exe"
+  "c__HW9.exe.manifest"
+  "libc__HW9.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c__HW9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
